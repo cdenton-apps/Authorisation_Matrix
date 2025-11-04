c69fc2c8d1e8ce8e5c1f3613276ed6697dfec8f4
cdenton-apps <connordenton@hotmail.co.uk>
2025-11-04 19:34:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from PIL import Image
 
 # -- Page config & branding
-st.set_page_config(page_title="Solidus Approval Finder", layout="wide")
+st.set_page_config(page_title="Financial Approval Finder", layout="wide")
 
 HIDE = """
 <style>
